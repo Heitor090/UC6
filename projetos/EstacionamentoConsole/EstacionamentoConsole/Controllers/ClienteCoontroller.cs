5fd8dd46e97494c922849926c2e03247a1b5c068
Heitor Leao <heitor.bleao@senacsp.edu.br>
2025-09-30 22:04:41 -0300

    Projeto 1

---
+++ projetos/EstacionamentoConsole/EstacionamentoConsole/Controllers/ClienteCoontroller.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstacionamentoConsole.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace EstacionamentoConsole.Controllers
+{
+    internal class ClienteCoontroller
+    {
+        EstacionamentoDbContext _context;
+
+        public ClienteCoontroller(EstacionamentoDbContext context)
+        {
+            _context = context;
+        }
+
+        public void ListarClientes()
+        {
+            Console.Clear();
+            var clientes = _context.Clientes.ToList();
+            foreach (var cliente in clientes)
+            {
+                Console.WriteLine($"ID: {cliente.Id}, Nome: {cliente.Nome}");
+            }
+            Console.WriteLine("\nPressione qualqe=uer tecla para retornar.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void AdicionarCliente()
+        {
+            Console.Clear();
+            Console.WriteLine("==== Adicionaar novo cliente ====");
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("CPF: ");
+            string cpf = Console.ReadLine();
+
+            Console.WriteLine("Telefone (Opicional): ");
+            string telefone = Console.ReadLine();
+
+
+            Cliente c1 = new Cliente(nome, cpf, telefone);
+            _context.Clientes.Add(c1);
+            _context.SaveChanges();
+
+            Console.WriteLine("Cliente adicionado com sucesso!");
+            Console.WriteLine("Aperte Qualquer Tecla Para Continuar");
+            Console.ReadKey();
+            Console.Clear();
+        }
+ 
+
+
+    public void VerDetalhesCliente()
+        {
+            Console.Clear();
+            Console.WriteLine("==== Detalhes do Cliente ====");
+            Console.Write("digite o ID do Cliente: ");
+            var clienteId = int.Parse(Console.ReadLine());
+
+            var cliente = _context.Clientes.FirstOrDefault( cliente => cliente.Id == clienteId);
+            if (cliente == null)
+                Console.WriteLine("Ciente não encontrado.");
+
+            else
+            {
+                Console.WriteLine($"ID: {cliente.Id}");
+                Console.WriteLine($"Nome: {cliente.Nome}");
+                Console.WriteLine($"CPF: {cliente.Cpf}");
+                Console.WriteLine($"Telefone: {cliente.Telefone}");
+
+            }
+            Console.WriteLine("\nAperte Qualquer Tecla Para Continuar");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ projetos/EstacionamentoConsole/EstacionamentoConsole/Program.cs
@@ -0,0 +1,83 @@
+﻿using EstacionamentoConsole.Controllers;
+using EstacionamentoConsole.Models;
+using EstacionamentoConsole.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+
+    .ConfigureServices((context, services) =>
+
+    {
+
+        services.AddDbContext<EstacionamentoDbContext>(opt =>
+
+            opt.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EstacionamentoDB;" +
+            "Trusted_Connection=True;TrustServerCertificate=True;"));
+        services.AddTransient<ClienteCoontroller>();
+
+    })
+
+    .Build();
+var clientesControllers = host.Services.GetRequiredService<ClienteCoontroller>();
+
+
+    bool sair = false;
+
+
+
+
+
+
+while (!sair) {
+    
+ 
+
+    Console.WriteLine("==== Sistema de Estacionamento ====");
+    Console.WriteLine("1. Listar Clientes");
+    Console.WriteLine("2. Adicionar Cliente");
+    Console.WriteLine("3. Ver Detalhes do Cliente");
+    Console.WriteLine("4. (A FAZER) Gerenciar Veiculos");
+    Console.WriteLine("5. (A FAZER) Gerenciar Vagas");
+    Console.WriteLine("0. Sair");
+    string opcao = Console.ReadLine();
+
+    Console.WriteLine($"Opção escoliha {opcao}");
+
+    switch (opcao)
+    {
+
+        case "1":
+            clientesControllers.ListarClientes();  
+            break;
+          
+        case "2":
+            clientesControllers.AdicionarCliente();
+            break;
+        case "3":
+            clientesControllers.VerDetalhesCliente();
+            break;
+        case "4":
+            Console.WriteLine("Chamou o Gerenciar Veiculos.");
+            break;
+        case "5":
+            Console.WriteLine("Chamou o Gerenciar Vagas.");
+            break;
+        case "0":
+            sair = true;
+            break;
+        default:
+          
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+    }
+}
+
+
+
+Console.WriteLine("Encerrando o sistema até logo");
