b47580f76015c82067b26a22681de98e127c61e6
Heitor Leao <heitor.bleao@senacsp.edu.br>
2025-10-27 21:38:43 -0300

    EstacionamentoSenac

---
+++ 04/EstacionamentoSenac/EstacionamentoSenac.API/Controllers/MotoristasController.cs
@@ -0,0 +1,82 @@
+﻿using EstacionamentoSenac.API.Data;
+using EstacionamentoSenac.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstacionamentoSenac.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotoristasController : ControllerBase
+    {
+        private AppDbContext _context;
+
+        public MotoristasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string Teste() => "Teste MOtoristaController";
+
+        [HttpGet]
+
+        public ActionResult<Motorista> GetMotoristas()
+        {
+            return Ok(_context.Motoristas.ToList());
+        }
+
+        [HttpGet("{ id }")]
+
+        public ActionResult<Motorista> GetMotoristaByID(int id)
+        {
+            var motorista = _context.Motoristas.Find(id);
+            if (id == null)
+                return NotFound();
+
+            return Ok(motorista);
+        }
+
+        [HttpPost]
+
+        public ActionResult<Motorista> PostMotorista(Motorista motorista)
+        {
+            _context.Motoristas.Add(motorista);
+            _context.SaveChanges();
+
+            return Created();
+
+        }
+
+        [HttpPut("{id}")]
+
+        public ActionResult<Motorista> PutMotorista(int id, Motorista motoristaNovo)
+        {
+            if (id != motoristaNovo.Id)
+                return BadRequest("Motorista informado na URL é diferente do arquivo Json");
+
+            var motoristaExistente = _context.Motoristas.Find(id);
+            if (motoristaExistente == null)
+                return NotFound();
+
+            motoristaExistente.Nome = motoristaNovo.Nome;
+
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
+
+        [HttpDelete]
+
+
+     public ActionResult<Motorista> DeleteMotorista(int id)
+        {
+            var motorista = _context.Motoristas.Find(id);
+            if (motorista == null) return NotFound();
+
+            _context.Motoristas.Remove(motorista);
+            _context.SaveChanges();
+            return NoContent();
+
+        }
+    }
+}
---
+++ 04/EstacionamentoSenac/EstacionamentoSenac.API/Models/Motorista.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EstacionamentoSenac.API.Models
+{
+    public class Motorista
+    {
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int? VeiculoId { get; set; }
+
+        [JsonIgnore]
+        public Veiculo? Veiculo { get; set;}
+    }
+}
