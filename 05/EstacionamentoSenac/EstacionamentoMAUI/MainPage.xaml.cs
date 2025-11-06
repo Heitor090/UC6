6b04c2c1e6d2db0666db61804835d862732d94fe
Heitor Leao <heitor.bleao@senacsp.edu.br>
2025-11-05 22:08:37 -0300

    Criando Telas para Aplicação

---
+++ 05/EstacionamentoSenac/EstacionamentoMAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EstacionamentoMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 05/EstacionamentoSenac/EstacionamentoMAUI/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace EstacionamentoMAUI.Models
+{
+    public class Veiculo
+    {
+     
+        public int Id { get; set; }
+
+        
+        public string Placa { get; set; }
+
+        public string? Modelo { get; set; }
+
+       
+        public string? Cor { get; set; }
+
+        [Required]
+        public int MotoristaId { get; set; }
+        public Motorista? Motorista { get; set; }
+    }
+}
