367b7fb149799a82cb768a2351fbf3fbcac28970
Heitor Leao <heitor.bleao@senacsp.edu.br>
2025-11-11 20:09:45 -0300

    Projeto de indicador

---
+++ Atividade De Indicador/EstacionamentoSenac/Estacionamento.MAUI/Models/Motorista.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.MAUI.Models
+{
+    public class Motorista
+    {
+        
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        
+        public string Cpf { get; set; }
+
+       
+        public string? Telefone { get; set; }
+
+        public ICollection<Veiculo> Veiculos { get; set; } = new List<Veiculo>();
+    }
+}
